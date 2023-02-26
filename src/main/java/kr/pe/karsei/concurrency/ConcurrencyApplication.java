b2f153155ad5ced9f6f0d3b5afcc35c61af6071d
Karsei <karseidev@gmail.com>
2023-02-26 23:50:18 +0900

    feat: zookeeper 환경 추가

--- src/main/java/kr/pe/karsei/concurrency/ConcurrencyApplication.java
+++ src/main/java/kr/pe/karsei/concurrency/ConcurrencyApplication.java
@@ -2,8 +2,10 @@ package kr.pe.karsei.concurrency;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableDiscoveryClient
 @EnableTransactionManagement
 @SpringBootApplication
 public class ConcurrencyApplication {
