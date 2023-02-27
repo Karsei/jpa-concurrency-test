7ca8c76238e5b8b4894c4564333a2b453bfc1c9c
Karsei <karseidev@gmail.com>
2023-02-27 23:59:40 +0900

    refact: 제한 추가

--- src/main/java/kr/pe/karsei/concurrency/item/mapper/ItemMapper.java
+++ src/main/java/kr/pe/karsei/concurrency/item/mapper/ItemMapper.java
@@ -2,7 +2,10 @@ package kr.pe.karsei.concurrency.item.mapper;
 
 import kr.pe.karsei.concurrency.item.domain.Item;
 import kr.pe.karsei.concurrency.item.entity.ItemJpaEntity;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public abstract class ItemMapper {
     public static Item mapItemPessimisticJpaToPessimistic(ItemJpaEntity entity) {
         return new Item(entity.getSeqNo(), entity.getName(), entity.getStock());
